07500a2bc1388111434d188a322b58d3311f2861
Huber-Ticona <super__xd@hotmail.com>
2021-10-30 10:36:30 -0300

    Html base y heredados creados. Uso de sesiones creada.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, url_for , request , render_template , session
+from flask import Flask, url_for , request , render_template, make_response , session
 from flask.wrappers import Request
 from flaskext.mysql import MySQL
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -12,16 +13,36 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'huber123'
 app.config['MYSQL_DATABASE_DB'] = 'madenco'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-
+app.secret_key="madenco"
 
 @app.route('/')
-def index():
-    return "<p>Hola >:3 bruh! xd  !</p>"
+def home():
+    if "usuario" in session:
+        usuario = session["usuario"]
+        return render_template('home.html' , nombre = usuario)
+    else:
+        return render_template('home.html' )
+    
 
-@app.route('/login' , methods = ['GET', 'POST'])   
+@app.route('/login' , methods = ['GET', 'POST'])   #iniciar sesión
 def login():
-    return render_template('base.html')
+    if request.method == 'POST':
+        nombre = request.form['nombre_usuario']
+        contra = request.form['contraseña']
+        session['usuario'] = nombre
+        return redirect(url_for("home"))
+    else:
+        return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('usuario', None)
+    return redirect(url_for('home'))
+
+@app.route('/panel-clasico')
+def panel_clasico():
+    return render_template('panel_clasico.html')
     
 @app.route('/mostrar/ordenes/')
 def ordenes():
@@ -39,10 +60,14 @@ def ordenes():
 def variable(dato):
     return f"hola, el dato es: {dato}"
 
+@app.errorhandler(404)
+def not_found():
+    """Page not found."""
+    return make_response(render_template("404.html"), 404)
 
 with app.test_request_context():
 
-    print(url_for('ordenes'))
+    print(url_for('home'))
     print(url_for('variable' , dato = 'huber ticona' ))
 
 if __name__ == '__main__':
